ceb68534770af240669ca5bb3dbc6ed016b00da6
gregm711 <miller.7285osu@gmail.com>
2022-02-06 22:45:13 -0500

    routing changes

--- pages/dao.tsx
+++ pages/dao.tsx
@@ -6,28 +6,17 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Link from "next/link";
 import { SendOneLamportToRandomAddress } from "./components/SendOneLamportToRandomAddress";
 
-const Home: NextPage = () => {
+const DAO: NextPage = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>Connect a wallet</h1>
-
-        <div className={styles.walletButtons}>
-          <WalletMultiButton />
-          <WalletDisconnectButton />
-        </div>
-
-        <div className={styles.grid}>
-          <Link href="/dao">
-            <a className={styles.card}>Start a dao</a>
-          </Link>
-        </div>
+        <h1 className={styles.title}>Start a dao</h1>
+        <div className={styles.grid}></div>
       </main>
     </div>
   );
 };
 
-export default Home;
+export default DAO;
